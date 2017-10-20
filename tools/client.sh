5733798f691cf8962087bbb09d37d95331998ed8
chenwin <383867485@qq.com>
2017-10-20 17:16:42 +0800

    Update client.sh
    输出多列csv

--- tools/client.sh
+++ tools/client.sh
@@ -42,22 +42,22 @@ title=""
 title1=""
 for len in ${tcp_len[@]}
 do
-    title="$title tcp_${len}_bw"
-	title1="$title1 tcp_${len}_lat"
+    title="$title,tcp_${len}_bw"
+	title1="$title1,tcp_${len}_lat"
 done
-title="$title $title1"
+title="$title,$title1"
 
 title2=""
 title3=""
 title4=""
 for len in ${udp_len[@]}
 do
-    title2="$title2 udp_${len}_bw"
-	title3="$title3 udp_${len}_lat"
-	title4="$title4 udp_${len}_pps"
+    title2="$title2,udp_${len}_bw"
+	title3="$title3,udp_${len}_lat"
+	title4="$title4,udp_${len}_pps"
 done
 #title2="$title2 $title3 $title4"
-title="$title $title4"
+title="$title,$title4"
 if [ -f $RESULT_HOME/net.csv ]
 then
     tmp=`cat $RESULT_HOME/net.csv | grep tcp`
@@ -74,13 +74,14 @@ for len in ${tcp_len[@]}
 do
 	tcp_bw=`tail -1 $RESULT_DIR/netperf_tcp_${len}.log | awk -F " " '{print $5}'`
     echo tcp_bandwidth_$len=$tcp_bw >> $RESULT_DIR/net_raw.csv
-    result="$result $tcp_bw"	
+    result="$result,$tcp_bw"	
    done
 for len in ${tcp_len[@]}
 do	
-	tcp_lat=`cat $RESULT_DIR/qperf_tcp_${len}.log | grep latency | awk -F "=" '{print $2}'|tr -d " "`
+	#tcp_lat=`cat $RESULT_DIR/qperf_tcp_${len}.log | grep latency | awk -F "=" '{print $2}'|tr -d " "`
+	tcp_lat=`cat $RESULT_DIR/qperf_tcp_${len}.log | grep latency | awk -F "=" '{print $2}'|awk -F "=" '{print $1}'`
 	echo tcp_latency_$len=$tcp_lat >> $RESULT_DIR/net_raw.csv	
-	result="$result $tcp_lat"
+	result="$result,$tcp_lat"
 done
 
 #for len in ${udp_len[@]}
@@ -102,7 +103,7 @@ do
 	udp_pps=`tac $RESULT_DIR/netperf_udp_${len}.log |sed -n 2p|awk -F " " '{print $3}'`
 	let udp_pps=$udp_pps/$TIME
 	echo udp_pps_$len=$udp_pps >> $RESULT_DIR/net_raw.csv
-	result="$result $udp_pps"
+	result="$result,$udp_pps"
 done
 
 echo $result >> $RESULT_HOME/net.csv
