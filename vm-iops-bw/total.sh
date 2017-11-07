c1b7303161c263d17a4558c185b5dfae79f0ad35
chenwin <383867485@qq.com>
2017-11-07 09:52:55 +0800

    Update total.sh

--- vm-iops-bw/total.sh
+++ vm-iops-bw/total.sh
@@ -1,9 +1,20 @@
+run_time=120
+times=0
+delay=$(($run_time+10))
+
 date_str=`date +"%Y%m%d_%H%M%S"`
 mkdir -p result/$date_str
 
 pkill fio
 
-for line in $(<dev.txt)
+for i in `seq 5`
 do
-  sh do.sh $date_str $line &
+  times=0
+  for line in $(<dev.txt)
+  do
+    sh do.sh $run_time $date_str $line $delay &
+        let times++
+  done
+  sleep $((($time+$delay)*6*$times+10*$times))
 done
+echo "=================done================="
